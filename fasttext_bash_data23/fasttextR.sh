118793e2297dcb1e3b8fcdcd08c752b2f1d534f1
Ricardo Fabián <143838204+007Richards@users.noreply.github.com>
2025-10-23 20:08:08 -0600

    Fix output file name and model name in script

--- fasttext_bash_data23/fasttextR.sh
+++ fasttext_bash_data23/fasttextR.sh
@@ -2,15 +2,15 @@
 
 train=/home/fasttext/Rest_Mex_train.csv
 test=/home/fasttext/Rest_Mex_test.csv
-res="ResultadosPol.txt"
+res="Resultados.txt"
 #numero de n-gramas "1-5":
 n=3
 #numero de epocas "5-50":
 epo=25
 #taza de aprendizaje "0.1-1.0":
 lr=1.0
-model="fasttext_model1"
+model="fasttext_model"
 pred="predicciones.txt"
 
 
-/home/rfabian/fastText/fasttext supervised -input $train -output $model -epoch $epo -lr $lr -wordNgrams $n && /home/fastText/ predict $model.bin $test 5 > $pred && /home/asttext test $model.bin $test > $res
+/home/fasttext supervised -input $train -output $model -epoch $epo -lr $lr -wordNgrams $n && /home/fastText/ predict $model.bin $test 5 > $pred && /home/asttext test $model.bin $test > $res
